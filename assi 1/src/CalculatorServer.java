bbd07f4453f8635325e0d13bb44312ffabbd74bc
Sakib Esrat <83289622+sakibesrat71@users.noreply.github.com>
2025-08-14 03:12:46 +0930

    Import issues resolved - check pom file

---
+++ assi 1/src/CalculatorServer.java
@@ -0,0 +1,18 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * CalculatorServer opens a RMI registry -> exports the calc,
+ * and binds it to the registry with the name "CalculatorService".
+ */
+public class CalculatorServer {
+    public static void main(String[] args) throws RemoteException {
+
+            CalculatorImplementation calc = new CalculatorImplementation();
+            Registry registry = LocateRegistry.createRegistry(1099); // Default RMI port
+            registry.rebind("CalculatorService", calc);
+            System.out.println("Calculator Server ready.");
+
+    }
+}
